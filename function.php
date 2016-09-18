630856ee356089a37f34b3af00ce12e554203c1f
Slamet Rivai <selampemayat@gmail.com>
2016-09-18 15:38:47 +0700

    add session
    add session

--- function.php
+++ function.php
@@ -1,8 +1,8 @@
 <?php
 woo_add_cart_fee() {
 global $woocommerce;
-$unik = rand(100, 999);
-$woocommerce->cart->add_fee( __('Unique Code', 'woocommerce'), $unik );
+$_SESSION['unik'] = 'date (i)';
+$woocommerce->cart->add_fee( __('Unique Code', 'woocommerce'), $_SESSION['unik'] );
 }
 add_action( 'woocommerce_cart_calculate_fees', 'woo_add_cart_fee' );
 ?>
